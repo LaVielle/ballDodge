1fa7790a6349742d8e43c0eea2836f95cfd563d4
LaVielle <Sam@Samuels-MacBook-Pro.local>
2017-03-14 15:22:38 -0400

    basic bootstrap layout

--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,14 @@
 
 
 function setup(){
-	canvas = createCanvas(800, 400);
+	canvas = createCanvas(700, 500);
+	canvas.parent('canvas-holder');
 	background(0);
 }
 
 function draw() {
 	background(0);
 	for (var  i = 0; i < 10; i ++){
-		ellipse(random(width), random(height), 5, 5)
+		//ellipse(random(width), random(height), 5, 5)
 	}
 }
