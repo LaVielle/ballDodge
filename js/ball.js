1326f434a3447d70749152c4c4309461ce0f131c
LaVielle <Sam@Samuels-MacBook-Pro.local>
2017-03-14 22:53:56 -0400

    added class and color to Ball object

--- js/ball.js
+++ js/ball.js
@@ -1,40 +1,43 @@
 
-function Ball(x, y, r, a){
+function Ball(x, y, r, a, c){
   this.pos = createVector(x, y);
     this.r = r;
     this.vel = createVector(0,0);
     this.accel = a;
+    this.class = c;
 
     this.update = function() {
       this.pos.y += this.accel;
-
     }
 
     this.eats = function(other){
     var d = p5.Vector.dist(this.pos, other.pos);
     if( d <= this.r +other.r){
-      this.r += other.r;
-      console.log("touched");
+      this.r += other.r*0.1;
       return true;
     }
     return false;
     }
 
     this.show = function() {
-      fill(255);
+      if (c == 1){
+        fill(255,0,0);
+      }
+      else if (c == 2){
+        fill(153,102,51);
+      }
+      else if (c == 3){
+        fill(0,0,255);
+      }
+      else if (c == 4){
+        fill(0,255,0);
+      }
+      else {
+        fill(255);
+      }
       ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
     }
 
 }
 
-function setup(){
-  canvas = createCanvas(700, 500);
-  canvas.parent('canvas-holder');
-  background(0);
 
-    for (var i = 0; i < 20; i++) {
-      var x = random(0,width);
-      var y = random(-height,0);
-      balls[i] = new ball2(x, y, 16);
-    }
-}
--- js/sketch.js
+++ js/sketch.js
@@ -14,8 +14,10 @@ function setup(){
   	for (var i = 0; i < 20; i++) {
 	    var x = random(0,width);
 	    var y = random(-height,0);
-	    balls[i] = new Ball(x, y, 16, 1);
+	    var c = Math.floor(random(1,5));
+	    balls[i] = new Ball(x, y, 16, 1, c);
   	}
+  	console.log(balls);
 }
 
 function draw() {
@@ -50,11 +52,39 @@ function draw() {
 		balls[i].update();
 
 		if(playerBall.eats(balls[i])){
-			balls.splice(i,1);
-			console.log("checked");
+			touched(playerBall,balls[i]);
+			balls[i].pos.x = random(width);
+			balls[i].pos.y = random(-height, -height/2);
+			//balls.splice(i,1);
 		}
 	}
 
 
 }
+
+function touched(b1, b2){
+	console.log("touched running");
+	if(b2.class == 1){
+		for (var i = 0; i < balls.length; i++) {
+			balls[i].accel += 1;
+		}
+	}
+	else if(b2.class == 2){
+		console.log("score -xx");
+	}
+	else if(b2.class == 3){
+		for (var i = 0; i < balls.length-1; i++) {
+			if (balls[i].accel <= 1){
+				balls[i].accel = 1;
+			}
+			else {
+				balls[i].accel -= 1;
+			}
+		}
+	}
+	else if (b2.class == 4){
+		console.log("score +xx");
+	}
+}
+
 
